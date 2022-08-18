2fb425abbd7d7abff455acdf2ec6519594493f51
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-08-18 08:07:08 -0300

    Update views.py

--- views.py
+++ views.py
@@ -7,17 +7,10 @@ def index(request):
         partes = request.split('\n\n')
         corpo = partes[1]
         params = {}
-        # Preencha o dicionário params com as informações do corpo da requisição
-        # O dicionário conterá dois valores, o título e a descrição.
-        # Posteriormente pode ser interessante criar uma função que recebe a
-        # requisição e devolve os parâmetros para desacoplar esta lógica.
-        # Dica: use o método split da string e a função unquote_plus
         for chave_valor in corpo.split('&'):
             chave, valor = chave_valor.split('=')
             params[chave] = unquote_plus(valor)
             print(params)
-    # Cria uma lista de <li>'s para cada anotação
-    # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
     note_template = load_template('note.html')
     notes_li = [
         note_template.format(title=dados['titulo'], details=dados['detalhes'])
