9de33fff17086df4f48d08e93f46d1d3f36856b3
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-08-23 08:36:23 -0300

    Create getit.js

---
+++ getit.js
@@ -0,0 +1,25 @@
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    let textareas = document.getElementsByClassName("autoresize");
+    for (let i = 0; i < textareas.length; i++) {
+      let textarea = textareas[i];
+      function autoResize() {
+        this.style.height = "auto";
+        this.style.height = this.scrollHeight + "px";
+      }
+      textarea.addEventListener("input", autoResize, false);
+    }
+    let cards = document.getElementsByClassName("card");
+    for (let i = 0; i < cards.length; i++) {
+      let card = cards[i];
+      card.className += ` card-color-${getRandomInt(
+        1,
+        5
+      )} card-rotation-${getRandomInt(1, 11)}`;
+    }
+  });
