c4cf1dfb950d1b36c7382a16a75c41d2f667083f
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-09-27 08:58:54 -0300

    Django version moved to another repository

--- Django version/notes/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Note(models.Model):
-    title = models.CharField(max_length=200)
-    details = models.TextField()
-    def __str__(self):
-        return '{}. {}'.format(self.id, self.title)
