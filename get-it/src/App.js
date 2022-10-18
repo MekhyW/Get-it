95e9b4f2b1e805b12233af7e11c84149e968a4a8
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-18 09:56:23 -0300

    Fonts

--- get-it/src/App.js
+++ get-it/src/App.js
@@ -4,7 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Note />
+      <Note title="Receita de miojo">
+        Bata com um martelo antes de abrir o pacote. Misture o tempero, coloque
+        em uma vasilha e aproveite seu snack :)
+      </Note>
+      <Note title="Sorvete de banana">
+        Coloque a banana no congelador e espere.
+      </Note>
     </div>
   );
 }
--- get-it/src/components/Note/index.js
+++ get-it/src/components/Note/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./index.css";
 
-export default function Note() {
+export default function Note(props) {
   return (
     <div className="card">
-      <h3 className="card-title">Receita de miojo</h3>
-      <div className="card-content">
-        <p>
-          Bata com um martelo antes de abrir o pacote. Misture o tempero,
-          coloque em uma vasilha e aproveite seu snack :)
-        </p>
-      </div>
+      <h3 className="card-title">{props.title}</h3>
+      <div className="card-content">{props.children}</div>
     </div>
   );
 }
