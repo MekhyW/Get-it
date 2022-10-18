13cfa3a5ea269021eab33df99bc038e8f45a0cd7
Mekhy W.! <55763383+MekhyW@users.noreply.github.com>
2022-10-18 10:19:27 -0300

    Card rotations + installed axios

--- get-it/src/App.js
+++ get-it/src/App.js
@@ -2,15 +2,27 @@ import Note from "./components/Note";
 import "./App.css";
 
 function App() {
+  const notes = [
+    {
+      id: 1,
+      title: "Receita de miojo",
+      content:
+        "Bata com um martelo antes de abrir o pacote. Misture o tempero, coloque em uma vasilha e aproveite seu snack :)",
+    },
+    {
+      id: 2,
+      title: "Sorvete de banana",
+      content: "Coloque a banana no congelador e espere.",
+    },
+  ];
+
   return (
     <div className="App">
-      <Note title="Receita de miojo">
-        Bata com um martelo antes de abrir o pacote. Misture o tempero, coloque
-        em uma vasilha e aproveite seu snack :)
-      </Note>
-      <Note title="Sorvete de banana">
-        Coloque a banana no congelador e espere.
-      </Note>
+      {notes.map((note) => (
+        <Note key={`note__${note.id}`} title={note.title}>
+          {note.content}
+        </Note>
+      ))}
     </div>
   );
 }
--- get-it/src/components/Note/index.js
+++ get-it/src/components/Note/index.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export default function Note(props) {
+  const [rotation, setRotation] = useState(0);
+  useEffect(() => {
+    setRotation(randomInt(-5, 5));
+  }, []);
+
+  const style = { transform: `rotate(${rotation}deg)` };
+
   return (
-    <div className="card">
+    <div className="card" style={style}>
       <h3 className="card-title">{props.title}</h3>
       <div className="card-content">{props.children}</div>
     </div>
